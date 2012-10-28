48413d242212199240bf81d04060f9dc4a1cccfe
Laurent Peuch <cortex@worlddomination.be>
2012-10-28 02:54:45 +0100

    [fix] put database settings in the settings.py of the project

--- settings.py
+++ settings.py
@@ -1,5 +1,15 @@
 from memopol.base.settings import *
 
+PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '%s/memopol2.sqlite' % PROJECT_PATH,
+    },
+}
+
+
 try:
     from settings_local import *
 except ImportError:
